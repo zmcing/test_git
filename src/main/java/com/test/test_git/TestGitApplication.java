1eaff17af5f8e4bf84ddb5dff0cfc6fc37f9f4cf
zhang <2234619844@qq.com>
2024-09-15 14:48:54 +0800

    Changes

---
+++ src/main/java/com/test/test_git/TestGitApplication.java
@@ -0,0 +1,14 @@
+package com.test.test_git;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestGitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestGitApplication.class, args);
+        System.out.println("aaa");
+    }
+
+}
