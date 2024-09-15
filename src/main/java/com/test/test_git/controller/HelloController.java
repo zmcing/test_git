c2147108cee46ce73e8541844f2f6a08b7ea78c8
zhang <2234619844@qq.com>
2024-09-15 15:02:37 +0800

    Changes

---
+++ src/main/java/com/test/test_git/controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.test.test_git.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String sayHello() {
+        System.out.println("hello");
+        return "hello";
+    }
+}
