03880ec21cb05fad7877c3bf3cf31a14c17ac61b
zhang <2234619844@qq.com>
2024-09-15 15:17:08 +0800

    Changes

---
+++ src/main/java/com/test/test_git/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.test.test_git.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String sayHello() {
+        System.out.println("test02");
+        return "Hello ";
+    }
+}
